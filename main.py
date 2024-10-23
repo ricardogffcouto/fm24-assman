1e639570b1fa1a1ca0e80907e28b608e975ff0b5
ricardo <ricardo.gff.couto@gmail.com>
2024-10-23 11:29:59 +0100

    Added wage, wage value

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+
+import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
@@ -65,13 +68,51 @@ def add_foot_scores(df):
     df['Footedness Score'] = df['Left Foot Score'] + df['Right Foot Score']
     return df
 
+
+def convert_wage(wage):
+    if pd.isna(wage) or wage == 'N/A':
+        return np.nan, np.nan
+    
+    # Remove all non-numeric characters except for periods and commas
+    wage_value_str = re.sub(r'[^\d.,]', '', wage)
+    
+    # If the string is empty after removing non-numeric characters, return NaN
+    if not wage_value_str:
+        return np.nan, np.nan
+    
+    # Replace commas with empty string and convert to float
+    try:
+        wage_value = float(wage_value_str.replace(',', ''))
+    except ValueError:
+        return np.nan, np.nan
+
+    # Determine the time period and calculate the yearly wage
+    if 'p/w' in wage:
+        yearly_wage = wage_value * 52
+    elif 'p/m' in wage:
+        yearly_wage = wage_value * 12
+    elif 'p/y' in wage:
+        yearly_wage = wage_value
+    else:
+        yearly_wage = np.nan  # Fallback if format is not recognized
+
+    return wage_value, yearly_wage
+
+def add_wage_value(df):
+    df['Wage Value'], df['Yearly Wage'] = zip(*df['Wage'].map(convert_wage))
+    return df
+
+
+
+
 def add_extra_attributes(df):
     df = add_foot_scores(df)
+    df = add_wage_value(df)
     return df
 
 # Example usage
 file_name = '2025-01 Youth'
 df = read_file(file_name)
 df = add_extra_attributes(df)
-print(df.head())
+print(df[['Name', 'Wage', 'Wage Value', 'Yearly Wage']].head())
 print(df.columns)  # Print column names to verify uniqueness
